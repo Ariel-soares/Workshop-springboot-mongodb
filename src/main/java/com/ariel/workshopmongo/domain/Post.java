93536c6c456ef589d49a3e3015cba1f985a72b62
Ariel Soares <ariel.sfranco@gmail.com>
2024-03-10 11:51:28 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/ariel/workshopmongo/domain/Post.java
+++ src/main/java/com/ariel/workshopmongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.ariel.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.ariel.workshopmongo.dto.AuthorDTO;
+import com.ariel.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable{
@@ -20,6 +23,7 @@ public class Post implements Serializable{
 	private String body;
 	
 	private AuthorDTO author;
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {}
 
@@ -64,11 +68,6 @@ public class Post implements Serializable{
 		this.body = body;
 	}
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
-
 	public AuthorDTO getAuthor() {
 		return author;
 	}
@@ -77,6 +76,19 @@ public class Post implements Serializable{
 		this.author = author;
 	}
 	
+
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
 	
 	@Override
 	public boolean equals(Object obj) {
@@ -89,8 +101,5 @@ public class Post implements Serializable{
 		Post other = (Post) obj;
 		return Objects.equals(id, other.id);
 	}
-
-	
-	
 
 }
